c9b0fe954c1bb904d3c40286ab0f0da73cbeb432
steve_d <tilma972@gmail.com>
2025-04-10 01:36:31 +0200

    Modification de la configuration pour export statique Next.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: 'out',
+    images: {
+      unoptimized: true,
+    },
+    // Autres configurations si nécessaires
+  }
+  
+  module.exports = nextConfig
